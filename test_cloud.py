74b4eb99a457780a4f4bf323af5de0c2cd44414b
Kris <user@Notebook1.(none)>
2013-11-27 14:08:39 +0100

    modif de test-cloud

--- test_cloud.py
+++ test_cloud.py
@@ -15,29 +15,38 @@ nb_serveurs = 0
 print "L'entree = %s " % E1.get()
 
 
-top1 = Tk()
-top2 = Tk()
-tab_label = []
+
+
+#tab_label = []
 tab_entry = []
 
 def choixServeur():
-	top2.mainloop()
+	top2 = Tk()
+	j=0
+	for i in tab_entry :
+		l = Label(top2,text=i.get())
+		l.pack(side = LEFT)
+		l.place(x=0,y=(j+1)*25)
+		j=j+1
+
 
 def entreeNomsServeurs():
 	if int(E1.get())>0:
 		nb_serveurs = E1.get()
 		tkMessageBox.showinfo( "Vous avez choisi : ", nb_serveurs+" serveur(s)")
+		top1 = Tk()
+		bouton = Tkinter.Button(top1,text="Envoyer",command=choixServeur)
+		bouton.pack()
 		for i in range(0,int(nb_serveurs)):
 			l = Label(top1, text="Nom du serveur num "+str(i+1)+" : ")
 			l.pack(side = LEFT)
 			l.place(x=0,y=(2*i+1)*25)
-			tab_label.append(l)
+			#tab_label.append(l)
 			e = Entry(top1,bd=2)
 			e.pack(side = RIGHT)
 			e.place(x=10,y=(i+1)*50)
 			tab_entry.append(e)
-	   	bouton = Tkinter.Button(top1,text="Envoyer",command=choixServeur)
-	   	top1.mainloop()
+		bouton.place(x=5,y=(int(nb_serveurs)+1)*50)
 
 B = Tkinter.Button(top, text ="Envoyer", command = entreeNomsServeurs)
 B.pack()
