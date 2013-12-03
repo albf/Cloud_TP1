71869c868027261b505a9584fbf5f81b0ef16164
Kris <user@Notebook1.(none)>
2013-12-03 09:11:01 +0100

    nouveaux boutons

--- test_cloud.py
+++ test_cloud.py
@@ -17,8 +17,10 @@ print "L'entree = %s " % E1.get()
 
 
 
-#tab_label = []
 tab_entry = []
+
+def afficherFichiers():
+	pass
 
 def choixServeur():
 	top2 = Tk()
@@ -26,26 +28,30 @@ def choixServeur():
 	for i in tab_entry :
 		l = Label(top2,text=i.get())
 		l.pack(side = LEFT)
-		l.place(x=0,y=(j+1)*25)
+		l.place(x=0,y=(2*j+1)*25)
+		b = Tkinter.Button(top2,text="Voir ses fichiers",command=afficherFichiers)
+		b.pack()
+		b.place(x=10,y=(j+1)*50)
 		j=j+1
 
 
-def entreeNomsServeurs():
-	if int(E1.get())>0:
-		nb_serveurs = E1.get()
-		tkMessageBox.showinfo( "Vous avez choisi : ", nb_serveurs+" serveur(s)")
-		top1 = Tk()
-		bouton = Tkinter.Button(top1,text="Envoyer",command=choixServeur)
-		bouton.pack()
-		for i in range(0,int(nb_serveurs)):
+def creationEntreesNomsServeurs(nb_serveurs,top1):
+	for i in range(0,int(nb_serveurs)):
 			l = Label(top1, text="Nom du serveur num "+str(i+1)+" : ")
 			l.pack(side = LEFT)
 			l.place(x=0,y=(2*i+1)*25)
-			#tab_label.append(l)
 			e = Entry(top1,bd=2)
 			e.pack(side = RIGHT)
 			e.place(x=10,y=(i+1)*50)
 			tab_entry.append(e)
+
+def entreeNomsServeurs():
+	if int(E1.get())>0:
+		nb_serveurs = E1.get()
+		top1 = Tk()
+		bouton = Tkinter.Button(top1,text="Envoyer",command=choixServeur)
+		bouton.pack()
+		creationEntreesNomsServeurs(nb_serveurs,top1)
 		bouton.place(x=5,y=(int(nb_serveurs)+1)*50)
 
 B = Tkinter.Button(top, text ="Envoyer", command = entreeNomsServeurs)
